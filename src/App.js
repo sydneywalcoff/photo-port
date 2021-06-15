921c02ef5e0e767162c2d92970f062c50cbbc197
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-06-14 17:31:45 -0700

    contact renders conditionally

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
   ]);
   
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
@@ -20,13 +21,18 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
         ></Nav>
       <main>
-        <ContactForm></ContactForm>
-        <Gallery
-          currentCategory={currentCategory}
-        ></Gallery>
-        <About></About>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+          )}
       </main>
     </div>
   );
